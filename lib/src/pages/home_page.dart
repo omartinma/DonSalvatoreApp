82cf846b3c48055f9f7ea6cdb8e7c15134505c49
LOPEZ SANCHEZ <cpd5845@grupofuertes.corp>
2020-05-11 13:49:57 +0200

    Commit 11/05/2020

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 String categoriaSeleccionada = "";
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,9 +11,9 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             ListaCategorias(),
+            // Contenedor que ocupa la pantalla entera menos 150 que es lo que le deja a ListaCategorias para darle su alto
             Container(
               height: MediaQuery.of(context).size.height - 150,
-              
             ),
           ],
         ),
@@ -23,7 +22,12 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class ListaCategorias extends StatelessWidget {
+class ListaCategorias extends StatefulWidget {
+  @override
+  _ListaCategoriasState createState() => _ListaCategoriasState();
+}
+
+class _ListaCategoriasState extends State<ListaCategorias> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -35,13 +39,46 @@ class ListaCategorias extends StatelessWidget {
           return Center(child: CircularProgressIndicator());
         }
 
+        // Tenemos los datos, cargamos la lista de categorias
         final List<dynamic> showData = json.decode(snapshot.data.toString());
         return Expanded(
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: showData.length,
             itemBuilder: (BuildContext context, int index) {
-              return CajaCategoria(showData[index]);
+              return GestureDetector(
+                onTap: () {
+                  print(showData[index]['nombreCategoria']);
+                  setState(() {
+                    categoriaSeleccionada = showData[index]['nombreCategoria'];
+                  });
+                },
+                child: Container(
+                  width: 100,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 6.0),
+                  decoration: BoxDecoration(
+                    color: (categoriaSeleccionada == showData[index]['nombreCategoria'])
+                        ? Colors.indigo[200]
+                        : Colors.yellow,
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset("assets/icon_pizza.png", height: 60.0),
+                      //TODO: FALTA CARGAR COLOR DE CONTAINER Y PATH DE IMAGEN DESDE EL JSON
+                      SizedBox(height: 5.0),
+                      Text(
+                        showData[index]['nombreCategoria'],
+                        style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              );
             },
           ),
         );
@@ -50,10 +87,14 @@ class ListaCategorias extends StatelessWidget {
   }
 }
 
+
+
+
+// No uso este Widget porque el SetState no em está funcionando al separar la lógica con él y no actualiza ListViweBuilder
 class CajaCategoria extends StatefulWidget {
   final dynamic categoriaDatos;
 
-  const CajaCategoria(this.categoriaDatos);
+  CajaCategoria(this.categoriaDatos);
 
   @override
   _CajaCategoriaState createState() => _CajaCategoriaState();
@@ -65,20 +106,19 @@ class _CajaCategoriaState extends State<CajaCategoria> {
     return GestureDetector(
       onTap: () {
         print(widget.categoriaDatos['nombreCategoria']);
-        
         setState(() {
           categoriaSeleccionada = widget.categoriaDatos['nombreCategoria'];
         });
-        print("........" + widget.categoriaDatos['nombreCategoria']);
       },
       child: Container(
         width: 100,
         margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
         decoration: BoxDecoration(
-          color: (categoriaSeleccionada == widget.categoriaDatos['nombreCategoria'])
+          color: (categoriaSeleccionada ==
+                  widget.categoriaDatos['nombreCategoria'])
               ? Colors.indigo[200]
               : Colors.yellow,
-          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -99,5 +139,3 @@ class _CajaCategoriaState extends State<CajaCategoria> {
     );
   }
 }
-
-
